b88c29df2507224c63a0d0b4e7d73a45017a4219
Vaskar Kundu <vaskar.kundu@nexdecade.com>
2024-02-19 16:58:06 +0600

    feat : added auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AuthModule } from './modules/auth/auth.module';
+import { MenuModule } from './modules/menu/menu.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, AppRoutingModule, AuthModule, MenuModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
