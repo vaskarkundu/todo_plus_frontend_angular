c2df77a2ea1d12bcc9d5e6b4ab87a6fa98da9cdf
Vaskar Kundu <vaskar.kundu@nexdecade.com>
2024-02-28 13:04:33 +0600

    feat : added signin component

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   {
-    path: '/',
+    path: '',
+    component: SigninComponent,
+  },
+  {
+    path: 'signup',
     component: SignupComponent,
   },
 ];
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 
 @NgModule({
   declarations: [
-    SignupComponent
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     CommonModule,
